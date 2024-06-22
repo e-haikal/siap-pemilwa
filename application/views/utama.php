f141e55be1b6aa1ba26fe19fd891684330f3e31e
e-haikal <taufanasman@yahoo.com>
2024-06-22 09:09:11 +0700

    minor: update Utama user

--- application/views/utama.php
+++ application/views/utama.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta name="description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+    <meta name="description" content="Vali is a responsive dan free admin theme built with Bootstrap 4, SASS dan PUG.js. It's fully customizable dan modular.">
     <!-- Twitter meta-->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:site" content="@pratikborsadiya">
@@ -13,7 +13,7 @@
     <meta property="og:title" content="Website Voting Pemilwa Amikom">
     <meta property="og:url" content="http://pratikborsadiya.in/blog/vali-admin">
     <meta property="og:image" content="http://pratikborsadiya.in/blog/vali-admin/hero-social.png">
-    <meta property="og:description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+    <meta property="og:description" content="Vali is a responsive dan free admin theme built with Bootstrap 4, SASS dan PUG.js. It's fully customizable dan modular.">
     <title>Siap Pemilwa - Web Pemilihan Suara</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,6 +22,12 @@
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/main.css'); ?>">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .card-img-top {
+            width: 100%;
+            height: auto;
+        }
+    </style>
 </head>
 
 <body class="app sidebar-mini">
@@ -48,23 +54,52 @@
             }
             ?>
         <div class="row">
-            <?php foreach ($kandidat as $data) : ?>
-                <div class="col mt-2">
+            <?php 
+            $count = count($kandidat);
+            foreach ($kandidat as $data) :
+                // Tentukan kelas berdasarkan jumlah kandidat
+                $col_class = ($count < 3) ? 'col-md-6' : 'col-lg-4 col-md-6';
+            ?>
+                <div class="<?= $col_class ?> col-sm-12 mt-2">
                     <div class="card">
                         <h3 class="card-header text-center"><?= $data->nomor_kandidat; ?></h3>
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><?= $data->nama_kandidat; ?></h5>
-                            <img src="<?= base_url('gambar/' . $data->foto_kandidat); ?>" style="height: 250px; width: 250px;" class="rounded mx-auto d-block" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title"><?= $data->nama_kandidat; ?></h5>
+                            <img src="<?= base_url('gambar/' . $data->foto_kandidat); ?>" style="height: 250px; width: 250px;" class="rounded mx-auto d-block mb-3" alt="Foto Kandidat">
+                            <button type="button" class="btn btn-info mt-3" data-toggle="modal" data-target="#visiMisiModal<?= $data->id_kandidat; ?>">
+                                Lihat Visi Misi
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- Modal -->
+                <div class="modal fade" id="visiMisiModal<?= $data->id_kandidat; ?>" tabindex="-1" role="dialog" aria-labelledby="visiMisiModalLabel<?= $data->id_kandidat; ?>" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="visiMisiModalLabel<?= $data->id_kandidat; ?>">Visi Misi <?= $data->nama_kandidat; ?></h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <?= $data->visi_misi; ?>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             <?php endforeach; ?>
-            <a class="btn btn-primary btn-block btn-lg mt-3" href="<?= base_url('utama/pilih'); ?>">Mulai Memilih</a>
         </div>
-
+        <a class="btn btn-primary btn-block btn-lg mt-3" href="<?= base_url('utama/pilih'); ?>">Mulai Memilih</a>
     </main>
     <?php $this->load->view('./_partials/bottom'); ?>
-
+    <!-- Bootstrap JS dan jQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
